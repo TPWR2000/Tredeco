297289889ff3ee5b0dd608cc29342ff12f41d5d1
Tomasz Pinas <tomasz.pinsa@gmail.com>
2026-02-26 20:19:37 +0100

    first commit

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.0g26grr9if8"
+    "revision": "0.ksmpj3pnst8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
